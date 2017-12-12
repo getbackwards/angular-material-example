eba5b031cbec66d9c16d1005dd9df37265f3324f
getbackwards <getbackwards@gmail.com>
2017-12-13 10:06:43 +1300

    last bit of exercise

---
+++ src/app/app-material/app-material.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import {
+  MatToolbarModule,
+  MatIconModule,
+  MatButtonModule,
+  MatMenuModule,
+  MatCardModule
+} from '@angular/material';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatCardModule
+  ],
+  declarations: []
+})
+export class AppMaterialModule { }
