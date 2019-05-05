59a0c03f19b3376047d97efb4549b230f3637305
tgray <tony.gray@trademe.co.nz>
2019-05-05 14:56:09 +1200

    did stuff yesterday

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  // templateUrl: './app.component.html',
+  templateUrl: './app.component.html',
   template: '<router-outlet></router-outlet>',
   styles: []
 })
-// export class AppComponent {
-//   title = 'app';
-// }
-export class AppComponent {}
+export class AppComponent {
+  title = 'app';
+}
+// export class AppComponent {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 
+import { HomeModule } from './home/home.module';
+
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 
@@ -15,7 +17,8 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
